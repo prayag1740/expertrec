c0ce169ee3d83e038d42809398ab614faaee6d9c
Prayag Bhatia <prayagbhatia@Prayags-MacBook-Air.local>
2021-03-11 08:43:10 +0530

    library name chnage

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name = 'myplexer_print_data',
+    name = 'myplexerlib',
     packages = find_packages(include=['myplexer_lib']),
     version = '0.1.0',
     description = 'My first python library',
