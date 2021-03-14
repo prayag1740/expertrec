267c4467a2dd761d858eb61a669b34ad65b494bd
Prayag Bhatia <prayagbhatia@Prayags-MacBook-Air.local>
2021-03-14 18:11:05 +0530

    changes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name = 'myplexerlib',
     packages = find_packages(include=['myplexer_lib']),
-    version = '0.1.0',
+    version = '0.2.0',
     description = 'My first python library',
     author = 'Prayag',
     license = 'MIT'
---
+++ test_myplexerfunc.py
@@ -0,0 +1,4 @@
+from myplexer_lib import myfunction
+
+input_string = "count prayag"
+print(myfunction.myplexer_print_data(input_string))
